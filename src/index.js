24324c573db4191fae74ff4f33579f12868e8e9a
sinbin1036 <ssb1036@naver.com>
2025-01-10 21:14:56 +0900

    01.10

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './proj02_102/App_page';
-import { BrowserRouter } from 'react-router-dom';
+import App from './winter_study/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App></App>
-  </BrowserRouter>
+  <App></App>
 );
