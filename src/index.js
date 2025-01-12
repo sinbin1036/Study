964d733dea60bf552e0fa31a171fa29f6da31ad9
sinbin1036 <ssb1036@naver.com>
2025-01-12 20:35:17 +0900

    1.12

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './proj_01/App';
+import App from './proj_news/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
